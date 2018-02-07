f1c6866b1c048b26e82446636698dd45231e77bb
Youssef Ali <youssefali6212@gmail.com>
2018-02-07 20:53:35 +0200

    Update BinarySearchTree_Insertion.java

--- Trees/BinarySearchTree_Insertion.java
+++ Trees/BinarySearchTree_Insertion.java
@@ -17,20 +17,18 @@ static Node Insert(Node root,int value) {
            if(root.right == null){
                root.right = new Node();
                root.right.data = value;
-               return root;
            }
            else{
-               Insert(root.right, value);
+               root.right = Insert(root.right, value);
            }
        }
        else if(root.data > value){
            if(root.left == null){
                root.left = new Node();
                root.left.data = value;
-               return root;
            }
            else{
-               Insert(root.left, value);
+               root.left = Insert(root.left, value);
            }
        }
        return root;
